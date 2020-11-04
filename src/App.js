12258c76787ddf7b2ea946312430b505170410ca
宋彪 <2889250797@qq.com>
2020-11-04 16:10:03 +0800

    移动的小球

--- src/App.js
+++ src/App.js
@@ -1,315 +1,50 @@
 /*
  * @Author: your name
  * @Date: 2020-10-15 18:55:48
- * @LastEditTime: 2020-10-26 10:40:40
+ * @LastEditTime: 2020-11-04 15:45:24
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \井字棋\my-app\src\App.js
- */
-import './App.css'
-import global from './global.js'
+ */ 
 
 import React, { Component } from 'react'
-class Content extends Component {
-  constructor(props) {
-    super(props)
-    this.boxRef = React.createRef();
-    this.state = {
-      base: [10, 10],
-      contentArr: [],
-      piece: 5,
-      count: 0,
-      position: [],
-    }
-  }
-  boxClick = (position) => {
-    this.state.contentArr.push(position)
-    this.state.position = [...position]
-    this.judgeWin(position)
-  }
-  judgeWin = (position) => {
-    let userArr = this.state.contentArr.filter((ele) => {
-      return ele[2] == position[2]
-    })
-    console.log(userArr)
-    if (userArr.length >= 2) {
-      let res = this.countNum(position, userArr)
-      if (res) {
-        setTimeout(()=>{
-          window.confirm(`${position[2]}赢了`);
-          console.log(this.boxRef,'boxref')
-          this.boxRef.current.clearContent();
-        },200)
-      }
-    }
-  }
-  countNum = (position, userArr) => {
-    let posiX = position[0]
-    let posiY = position[1]
-    let winState = false
-    // 横向判断
-    if (!winState) {
-      this.state.count = 0
-      for (let i = posiY - 1; i >= 0; i--) {
-        if (
-          userArr.some((ele) => {
-            return ele[0] == posiX && ele[1] == i
-          })
-        ) {
-          console.log('success')
-          this.state.count++
-          if (this.state.count == this.state.piece) {
-            winState = true
-            return true
-          }
-        } else {
-          console.log('fail1')
-          break
-        }
-      }
-      for (let i = posiY; i < this.state.base[1]; i++) {
-        if (
-          userArr.some((ele) => {
-            return ele[0] == posiX && ele[1] == i
-          })
-        ) {
-          console.log('success1')
-          this.state.count++
-          if (this.state.count == this.state.piece) {
-            winState = true
-            return true
-          }
-        } else {
-          console.log('fail2')
+import MoveBar from './components/moveBar'
 
-          break
-        }
-      }
+function createBar(arr){ 
+  console.log(arr);
+  return arr.map((ele)=>{
+    let math = (Math.random() + 0.5).toFixed(2) ;
+    let colora = Math.random() * 256;
+    let colorb = Math.random() * 256;
+    let colorc = Math.random() * 256;
+    let opacity = Math.random() * 0.7 + 0.1;
+    let top = Math.random() * 600;
+    let left = Math.random() * 1200;
+    let speedX = Math.random() + 0.5;
+    let speedY = Math.random() + 0.5;
+    return {
+      background:`rgba(${colora},${colorb},${colorc},${opacity})`,
+      width:math * 100,
+      height:math * 100,
+      top:top,
+      left:left,
+      speedX:speedX,
+      speedY:speedY
     }
-    // 纵向判断
-    if (!winState) {
-      this.state.count = 0
-      for (let i = posiX - 1; i >= 0; i--) {
-        if (
-          userArr.some((ele) => {
-            return ele[0] == i && ele[1] == posiY
-          })
-        ) {
-          console.log('success')
-          this.state.count++
-          if (this.state.count == this.state.piece) {
-            winState = true
-            return true
-          }
-        } else {
-          console.log('fail1')
-          break
-        }
-      }
-      for (let i = posiX; i < this.state.base[0]; i++) {
-        if (
-          userArr.some((ele) => {
-            return ele[0] == i && ele[1] == posiY
-          })
-        ) {
-          console.log('success1')
-          this.state.count++
-          if (this.state.count == this.state.piece) {
-            winState = true
-            return true
-          }
-        } else {
-          break
-        }
-      }
-    }
-    // 斜向1判断
-    if (!winState) {
-      this.state.count = 0
-      let x = position[0]
-      let y = position[1]
-      while (x >= 0 && y >= 0) {
-        if (
-          userArr.some((ele) => {
-            return ele[0] == x && ele[1] == y
-          })
-        ) {
-          this.state.count++
-          x--
-          y--
-          if (this.state.count == this.state.piece) {
-            winState = true
-            return true
-          }
-        } else {
-          break
-        }
-      }
-      x = position[0]
-      y = position[1]
-      while (x < this.state.base[1] && y < this.state.base[1]) {
-        if (
-          userArr.some((ele) => {
-            return ele[0] == x + 1 && ele[1] == y + 1
-          })
-        ) {
-          this.state.count++
-          x++
-          y++
-          if (this.state.count == this.state.piece) {
-            winState = true
-            return true
-          }
-        } else {
-          break
-        }
-      }
-    }
-    // 斜向2判断
-    if (!winState) {
-      this.state.count = 0
-      let x = position[0]
-      let y = position[1]
-      while (x >= 0 && y < this.state.base[1]) {
-        if (
-          userArr.some((ele) => {
-            return ele[0] == x && ele[1] == y
-          })
-        ) {
-          this.state.count++
-          x--
-          y++
-          if (this.state.count == this.state.piece) {
-            winState = true
-            return true
-          }
-        } else {
-          break
-        }
-      }
-      x = position[0]
-      y = position[1]
-      while (x < this.state.base[1] && y >= 0) {
-        if (
-          userArr.some((ele) => {
-            return ele[0] == x + 1 && ele[1] == y - 1
-          })
-        ) {
-          this.state.count++
-          x++
-          y--
-          if (this.state.count == this.state.piece) {
-            winState = true
-            return true
-          }
-        } else {
-          break
-        }
-      }
-    }
-    console.log(winState)
-    return winState
-  }
-  renderBox(index) {
-    let boxArr = []
-    for (let i = 0; i < this.state.base[1]; i++) {
-      boxArr.push(
-        <Box
-          ref={this.boxRef}
-          position={[index, i]}
-          key={index + i}
-          boxClick={this.boxClick}
-        > 
-        </Box>
-
-      )
-    }
-    return boxArr
-  }
-  render() {
-    let rendArr = []
-    for (let i = 0; i < this.state.base[0]; i++) {
-      rendArr.push(
-        <div className="boxWrapper" key={i}>
-          {this.renderBox(i)}
-        </div>
-      )
-    }
-    return <div> {rendArr} </div>
-  }
+  })
 }
-
-class Box extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      user: '',
-      position: '',
-      className:'box'
-    }
-  }
-  repaint() { }
-  boxClick = () => {
-    if (this.state.position) return
-    global.currentUser++
-    this.setState(
-      {
-        position: this.props.position,
-        user: global.currentUser % 2 == 0 ? '玩家1' : '玩家2',
-      },
-      () => {
-        console.log(this.state)
-        this.repaint()
-        this.props.boxClick([...this.props.position, this.state.user])
-      }
-    )
-  }
-  clearContent = () => {
-    console.log('触发了');
-    this.setState(
-      ()=>{
-        return {
-          position: [],
-          user: '',
-        }
-      }
-    ) 
-    this.setState(()=>{
-        return {
-          className:this.state.user == '' ? 'box' : this.state.className
-        }
-      }) 
-
-  }
-  returnClass() {
-    console.log('执行了returnclass');
-    console.log(this.state.user,'user');
-    if (this.state.user == '玩家1'){
-      this.state.className = 'box red'
-    }
-    if (this.state.user == '玩家2'){
-      this.state.className = 'box blue'
-    }
-    return '';
-  }
-  render(){
-    console.log('执行了render',['box', this.returnClass()].join(' '));
-    // const abc = ['box', this.returnClass()].join(' ');
-    return (
-      <div
-        className={this.state.className}
-        onClick={this.boxClick}
-      ></div>
-    )
-  }
-}
-
-function App() {
-  console.log(Content)
+function App() { 
+  let arr = new Array(10).fill(1);
+  let newArr = createBar(arr);
+  console.log(newArr);
   return (
     <div className="App">
-      <Content> </Content>
+      {
+        newArr.map((item,index)=>{
+          return <MoveBar key={index} {...item}></MoveBar>
+        })
+      }
+      
     </div>
   )
 }
---
+++ src/components/moveBar.js
@@ -0,0 +1,87 @@
+/*
+ * @Author: your name
+ * @Date: 2020-11-04 11:37:59
+ * @LastEditTime: 2020-11-04 15:35:15
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \react项目练习\my-app\src\components\moveBar.js
+ */
+import React, { Component } from 'react'
+
+export default class moveBar extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            top:this.props.top || 0,
+            left:this.props.left || 0,
+            width:this.props.width || 100,
+            height:this.props.height || 100,
+            background:this.props.background || 'red',
+            speedX:this.props.speedX || 1,
+            speedY:this.props.speedY || 2
+        }
+        this.style = {
+            width:this.state.width + 'px',
+            height:this.state.height + 'px',
+            borderRadius:'50%',
+            backgroundColor:this.state.background,
+            position:'absolute',
+            top:this.state.top + 'px',
+            left:this.state.left + 'px',
+        }
+        this.timer = null;
+    }
+    move=()=>{
+        this.setState(()=>{
+            return {
+                top:this.state.top + this.state.speedY,
+                left:this.state.left + this.state.speedX,
+            }
+        },()=>{
+            this.style = {
+                width:this.state.width + 'px',
+                height:this.state.height + 'px',
+                borderRadius:'50%',
+                backgroundColor:this.state.background,
+                position:'absolute',
+                top:this.state.top + 'px',
+                left:this.state.left + 'px',
+            } 
+            if(this.state.top >= document.body.clientHeight - this.state.height){
+                this.setState({
+                    speedY : -(this.state.speedY),
+                    top:document.body.clientHeight - this.state.height
+                })
+            }
+            if(this.state.top <= 0){
+                this.setState({
+                    speedY : -(this.state.speedY),
+                    top:0,
+                })
+            }
+            if(this.state.left >= document.body.clientWidth - this.state.width){
+                this.setState({
+                    speedX : -(this.state.speedX),
+                    left:document.body.clientWidth - this.state.width,
+                })
+            }
+            if(this.state.left <= 0){
+                this.setState({
+                    speedX : -(this.state.speedX),
+                    left:0,
+                })
+            }
+        })
+    }
+    render() {
+        if(this.timer) cancelAnimationFrame(this.timer);
+        this.timer = requestAnimationFrame(()=>{
+            this.move()
+        })
+        return (
+            <div className="bar" style={this.style}>
+                
+            </div>
+        )
+    }
+}
